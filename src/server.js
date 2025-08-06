0287531d21794c11b089b4f02f4ed3a02a41c1d3
Kwesi-ux <kwesi.amaning@acity.edu.gh>
2025-08-06 12:48:16 +0000

    First Commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import dotenv from "dotenv"
+import { initDB } from "./config/db.js"
+import ratelimiter from "./middleware/rateLimiter.js"
+import transactionsRoute from "./routes/transactionsRoute.js"
+
+dotenv.config()
+
+const app = express()
+app.use(ratelimiter)
+app.use(express.json())
+
+const PORT = process.env.PORT
+
+
+
+
+
+app.use("/api/transactions", transactionsRoute)
+
+
+initDB().then( () => {
+    app.listen(PORT, () => {
+        console.log("Server is running on PORT:", PORT)
+    })
+})
